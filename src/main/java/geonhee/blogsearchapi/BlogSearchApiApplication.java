59082b07b41cee228d67dc2a679af81ae0499987
geony <jvckigh@gmail.com>
2023-03-18 13:17:29 +0900

    Init project

---
+++ src/main/java/geonhee/blogsearchapi/BlogSearchApiApplication.java
@@ -0,0 +1,15 @@
+package geonhee.blogsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class BlogSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/geonhee/blogsearchapi/config/Swagger2Config.java
@@ -0,0 +1,38 @@
+package geonhee.blogsearchapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+//                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("geonhee.blogsearchapi"))
+                // 스웨거가 RestController를 전부 스캔을 한다.
+                // basePackage => 어디를 범위로 스캔을 할 것인지 작성
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("블로그 조회 API 서비스")
+                .description("블로그 조회 API 서비스 입니다")
+                .version("0.0.1")
+                .termsOfServiceUrl("https://antstudy.tistory.com/")
+                .license("LICENSE")
+                .licenseUrl("")
+                .build();
+    }
+}
